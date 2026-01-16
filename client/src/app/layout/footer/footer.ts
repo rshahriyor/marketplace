6f1b7b78afdb8abcc147a63cffce7c9769284575
Shahriyor <rshahriyor@Shahriyors-MacBook-Air.local>
2026-01-16 13:04:07 -0500

    refactoring

--- client/src/app/layout/footer/footer.ts
+++ client/src/app/layout/footer/footer.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
 
+interface IMenuItem {
+  label?: string;
+  id?: number;
+  icon?: string;
+  src?: string;
+  routerLink?: string;
+  linkType?: string;
+  open?: boolean;
+  children?: IMenuItem[]
+}
+
 @Component({
   selector: 'mk-footer',
   imports: [],
@@ -7,5 +18,40 @@ import { Component } from '@angular/core';
   styleUrl: './footer.css',
 })
 export class Footer {
+  menuListItems: IMenuItem[] = [
+    {
+      label: 'Оферта'
+    },
+    {
+      label: 'Лицензия'
+    },
+    {
+      label: 'Договоры'
+    },
+    {
+      label: 'О платформе'
+    }
+  ]
+  socialMediaListItems: IMenuItem[] = [
+    {
+      label: 'Instagram',
+      icon: '/assets/social/instagram.svg',
+      src: 'https://www.instagram.com/markets_tj.official?igsh=amZyc2YwMWpycjky'
+    },
+    {
+      label: 'Facebook',
+      icon: '/assets/social/facebook.svg'
+    },
+    {
+      label: 'Telegram',
+      icon: '/assets/social/telegram.svg',
+      src: 'https://t.me/markets_tj_official_channel'
+    },
+    {
+      label: 'WhatsApp',
+      icon: '/assets/social/whatsapp.svg',
+      src: 'https://wa.me/992115552555'
+    }
+  ]
 
 }
